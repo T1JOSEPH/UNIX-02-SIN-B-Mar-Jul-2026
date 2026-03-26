6d226e788d2d66971d485864af1f4c0a86b95143
T1JOSEPH <jovacame@uide.edu.ec>
2026-03-26 16:51:20 +0000

    commit 5

--- practicalworkchop.sh
+++ practicalworkchop.sh
@@ -8,10 +8,10 @@ cat mi_llave_publica.asc        #Muestra el contenido de la clave pública en pa
 gpg --list-secret-keys --keyid-format=long   
 gpg --armor --export-secret-keys        #Exporta tus claves privadas en formato texto (uso delicado)
 gpg --import mi_llave_publica_Arthur.asc     #Importa la llave publica de alguien mas 
-echo "Hola que hace" > doc_no_cifrado.txt   #"Muestra con echo el mensaje, y lo crea en un documento nuevo"
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txtecho "Hola que hace" > doc_no_cifrado.txt   #"Muestra con echo el mensaje, y lo crea en un documento nuevo"
 gpg --output doc_cifrado.txt --encrypt --recipient beltranartfire@hotmail.com "hash o correo de la persona" doc_no_cifrado.txt       
 #Cifra el archivo usando la clave pública del destinatario y genera un archivo encriptado que solo esa persona puede descifrar
 #output especifica el nombre del archivo donde se guardará el resultado del comando
 gpg --decrypt doc_cifrado.txt       #desenecripta 
-gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt
 cat doc_no_cifrado_firmado.txt
+gpg --verify doc_no_cifrado_firmado_Arthur.txt
