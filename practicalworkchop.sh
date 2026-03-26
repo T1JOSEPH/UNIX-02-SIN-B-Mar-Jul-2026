e53c0104685f318d00ed321fe1254d9e8c4067a6
T1JOSEPH <jovacame@uide.edu.ec>
2026-03-26 02:59:57 +0000

    commit 1

---
+++ practicalworkchop.sh
@@ -0,0 +1,4 @@
+uname -a    #Muestra información del sistema (kernel, arquitectura, etc.)
+which gpg   #Indica la ruta donde está instalado gpg
+gpg --version       #Muestra la versión de GPG y sus características
+gpg --full-generate-key     #Crea un nuevo par de claves (pública y privada)
