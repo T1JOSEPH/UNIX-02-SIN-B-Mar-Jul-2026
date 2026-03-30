6514abf01bd6fbaca0e09a45ad5133dd5561c09d
T1JOSEPH <jovacame@uide.edu.ec>
2026-03-30 13:07:47 +0000

    fs 4

--- practical_class2.sh
+++ practical_class2.sh
@@ -1,7 +1,8 @@
-sudo apt update
-sudo apt upgrate
-sudo apt install parted
-sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e
+sudo apt update #Aquí lo que hago es actualizar la lista de paquetes del sistema.
+sudo apt upgrade #Aquí hay un error: debería ser upgrade.
+sudo apt install parted #Aquí estoy instalando parted, que es una herramienta para ver discos y/o las partciones
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Ejecuta varios comandos en cadena para analizar el disco, pero de forma ordenada.
+
 
 
 
