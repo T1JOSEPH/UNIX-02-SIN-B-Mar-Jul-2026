36d719b8549f759665efa2b72febc46ff8986695
T1JOSEPH <jovacame@uide.edu.ec>
2026-03-30 13:52:29 +0000

    fs6

--- practical_class2.sh
+++ practical_class2.sh
@@ -3,8 +3,8 @@ sudo apt upgrade # This command upgrades all installed packages to their latest 
 sudo apt install parted #Aquí estoy instalando part# Here I install parted, which is a tool used to manage disks and partitions.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Executes multiple commands in sequence to analyze the disk in an organized way.
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #This checks if the system is using UEFI or BIOS
-
-
+echo "esto es un archivo" > archivo.txt #Creates a file named archivo.txt and writes the text "esto es un archivo" into it, overwriting the file if it already exists.
+stat archivo.txt #Displays detailed information about the file, such as its size, permissions, and timestamps.
 
 #Estudio
 parted -l
