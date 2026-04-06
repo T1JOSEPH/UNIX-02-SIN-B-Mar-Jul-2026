7505c5fd475bbf29fc8d607b8dedebba84070705
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-06 12:37:44 +0000

    commit 2 we practice how to create a folder, fix a mistake for misspelling, and view the contents in detail in the terminal.

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,6 @@ cd / #takes us to the root directory
 cd /home/codespace #moves us to a specific path in the system.
 cd ~ #goes directly to the user’s home directory.
 cd $HOME #does the same as cd ~, using a system variable that points to the home directory.
+mkdir proyecto #created a new folder named “proyecto”.
+cd proyecto/ #correctly entered the folder.
+ls -lai #isted all files in the directory with detailed information
