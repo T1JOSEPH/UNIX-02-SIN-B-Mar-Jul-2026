b11f4cd4282f6d4c6b4894d0e0c0a2b0f0a063d1
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-06 12:41:07 +0000

    result

--- script_boot.sh
+++ script_boot.sh
@@ -5,3 +5,6 @@ cd $HOME #does the same as cd ~, using a system variable that points to the home
 mkdir proyecto #created a new folder named “proyecto”.
 cd proyecto/ #correctly entered the folder.
 ls -lai #isted all files in the directory with detailed information
+#result
+1572927 drwxrwxrwx+ 2 codespace codespace 4096 Apr  6 12:34 .
+1572874 drwxrwxrwx+ 4 codespace root      4096 Apr  6 12:34 .. #This result shows the information of the current directory and its parent directory after using ls -lai, including basic details such as permissions, owner and date, confirming that we are inside the created folder and viewing its contents
