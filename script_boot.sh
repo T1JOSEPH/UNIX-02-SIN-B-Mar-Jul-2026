d9cb191e986af8bea9cbf13922bd3f6d740bbba4
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-07 16:50:02 +0000

    I learned how to compile the Linux kernel from source code.
    First, I understood that the Linux kernel is the core of the operating system, and compiling it means building it from its original code.

--- script_boot.sh
+++ script_boot.sh
@@ -28,7 +28,8 @@ ls -la #shows all files (including hidden ones)
 sudo apt update #update the list of available packages.
 sudo apt upgrade #install updates for already installed packages.
 sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
-#install several necessary tools
+#install several necessary tools 
 git clone --depth 1 https://github.com/torvalds/linux.git #downloads the Linux kernel source code quickly
 cd linux #enters the downloaded project folder.
 make menuconfig #opens an interactive menu to configure the kernel before building it.
+make -j 2 #is a command used to compile
