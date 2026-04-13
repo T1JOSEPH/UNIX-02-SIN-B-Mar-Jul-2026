f8613d496b83a3164fe5f57b02b8db890519d8f9
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-13 12:40:18 +0000

    BootEX2

--- script2_boot.sh
+++ script2_boot.sh
@@ -1,3 +1,6 @@
 ls -a #shows all files, including hidden ones
 ls --all #same as -a
 ls -a / #shows everything in the root /, including hidden files
+ls -l -a -ah #detailed list, includes hidden files, human-readable sizes   
+ls -l -ah #same, but may not include all hidden files explicitly (depends on -a)
+ls -lah #same as combining all: detailed, hidden files, readable sizes
