d43974789635e3346c3b85d6b2dddc8ab88a5e59
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-13 12:59:54 +0000

    BootEX3

--- script2_boot.sh
+++ script2_boot.sh
@@ -4,3 +4,9 @@ ls -a / #shows everything in the root /, including hidden files
 ls -l -a -ah #detailed list, includes hidden files, human-readable sizes   
 ls -l -ah #same, but may not include all hidden files explicitly (depends on -a)
 ls -lah #same as combining all: detailed, hidden files, readable sizes
+mkdir -- -rf #creates a folder named -rf
+rm -- -rf #deletes file/folder named -rf
+rmdir -- -rf #deletes folder -rf (empty)
+ls --help #quick help
+man ls #full manual
+q #exit the manual
