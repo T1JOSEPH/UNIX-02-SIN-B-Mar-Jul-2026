1ee44f24424841376aa175b90e9d08fc21e71496
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-15 13:04:00 +0000

    ejercicio1.1

---
+++ ejercicio1.sh
@@ -0,0 +1,3 @@
+echo '#!/bin/sh' > hola.sh #It creates a file named hola.sh and writes #!/bin/sh into it, which defines the script interpreter
+echo 'echo "Hola desde mi primer script"' >> hola.sh #It appends the line echo "Hola desde mi primer script" to hola.sh without overwriting the existing content.
+cat hola.sh #It displays the content of the file hola.sh on the screen
