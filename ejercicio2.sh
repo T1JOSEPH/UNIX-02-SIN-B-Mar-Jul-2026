90f121aa3c37bad94ba26aa92b3921c01e93cd54
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-15 13:37:40 +0000

    ejercicio2.3

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,2 +1,3 @@
 touch prueba.txt #create an empty file
 chmod 600 prueba.txt #Sets read and write permissions only for the owner, no access for others.
+ls -l prueba.txt #Shows detailed information about prueba.txt, including its permissions
