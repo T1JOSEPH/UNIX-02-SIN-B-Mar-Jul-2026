4a0d7988fccf5d01b17f378ca2b5ee24e2fa2c8a
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-15 13:07:58 +0000

    ejercicio1.3

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,3 +2,4 @@ echo '#!/bin/sh' > hola.sh #It creates a file named hola.sh and writes #!/bin/sh
 echo 'echo "Hola desde mi primer script"' >> hola.sh #It appends the line echo "Hola desde mi primer script" to hola.sh without overwriting the existing content.
 cat hola.sh #It displays the content of the file hola.sh on the screen
 .hola.sh #It runs the script hola.sh from the current directory.
+chmod +x hola.sh #since I do not execute the previous command with this one, we give it permission
