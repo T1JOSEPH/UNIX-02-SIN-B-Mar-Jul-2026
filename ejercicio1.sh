3011c19216557524cdf1b6c0faeb476bc0f97994
T1JOSEPH <jovacame@uide.edu.ec>
2026-04-15 13:08:43 +0000

    ejercicio1.4

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -3,3 +3,4 @@ echo 'echo "Hola desde mi primer script"' >> hola.sh #It appends the line echo "
 cat hola.sh #It displays the content of the file hola.sh on the screen
 .hola.sh #It runs the script hola.sh from the current directory.
 chmod +x hola.sh #since I do not execute the previous command with this one, we give it permission
+./hola.sh #now if you run it without problems
