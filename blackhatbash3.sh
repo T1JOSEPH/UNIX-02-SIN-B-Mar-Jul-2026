6023937b7f25be36f30c532ece96bc2c49d786a9
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-01 13:44:28 +0000

    root_directory=$(ls -ld /) → This command stores the result of ls -ld / inside a variable called root_directory. It does not display anything on the screen because the output is being saved.
    echo "${root_directory}" → This command displays the content stored in the variable. In this case, it shows the information of the root directory /.

---
+++ blackhatbash3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+echo "This book's name is Linux"
+
+set +x
+
+$ echo "This book's name is ${book}"
+This book's name is black hat bash
+
+[Jose] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash -x blackhatbash3.sh
++ set -x
++ echo 'This book'\''s name is Linux'
+This book's name is Linux
++ set +x
+
+#resultados
+$ root_directory=$(ls -ld /)
+$ echo "${root_directory}"
+
+[Jose] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ echo "${root_directory}"
+drwxr-xr-x 1 root root 4096 Jun  1 12:27 /
+[Jose] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ 
