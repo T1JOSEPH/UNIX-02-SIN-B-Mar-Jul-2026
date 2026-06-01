1f6f04164bb9b5fd56b3e81b6a17dffba7986983
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-01 13:35:58 +0000

    resultados finales

--- blackhatbash2.sh
+++ blackhatbash2.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# All this script does is create a directory, create a file
-# within the directory, and then list the contents of the di
 set -x
-rectory.
+
+# All this script does is create a directory, create a file within the directory, and then list the contents of the directory.
+
 mkdir mydirectory
 touch mydirectory/myfile
 ls -l mydirectory
+
 set +x
 
 [Jose] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash -x blackhatbash2.sh
 + set -x
-+ rectory.
-blackhatbash2.sh: line 5: rectory.: command not found
 + mkdir mydirectory
 + touch mydirectory/myfile
 + ls -l mydirectory
 total 0
--rw-rw-rw- 1 vscode vscode 0 Jun  1 13:27 myfile
+-rw-rw-rw- 1 vscode vscode 0 Jun  1 13:34 myfile
 + set +x
