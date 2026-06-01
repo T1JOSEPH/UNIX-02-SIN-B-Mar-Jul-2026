4b78f0196b5bd2f2d1f11df34a7bbad7c8c2bc19
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-01 12:54:14 +0000

    Description

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,9 +1,9 @@
-bash --version
-env
-echo ${SHELL}
-echo ${RANDOM}
-echo &{UID}
-echo ${OSTYPE}
-ps -e -f
-ps -ef
-df --human-readable
+bash --version #Shows the installed Bash version on the system.
+env #Displays all environment variables currently configured
+echo ${SHELL} #Shows the current shell being used.
+echo ${RANDOM} #Generates and displays a random number.
+echo &{UID} #Tries to display the user ID, but this syntax is incorrect in Bash.
+echo ${OSTYPE} #Displays the operating system type.
+ps -e -f #Shows all running processes with detailed information.
+ps -ef #Also displays all active processes in full format.
+df --human-readable #Shows disk space usage in a readable format like MB or GB.
