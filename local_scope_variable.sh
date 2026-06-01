d782cf88c58a2fba8c958d45727274ea1e375011
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-01 13:55:47 +0000

    explitacion

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PUBLISHER="No Starch Press"
+PUBLISHER="No Starch Press" Globalvariable #Works in the entire script. It can be used inside and outside functions.
 print_name(){
-local name
+local name        Localvariable #Works only inside the function where it was created. Outside the function, it cannot be accessed.
 name="Black Hat Bash"
-echo "${name} by ${PUBLISHER}"
+echo "${name} by ${PUBLISHER}"   Infoke Fuction #A function is a reusable block of commands in Bash. It helps organize code and avoid repeating commands.
 }
 print_name
 echo "Variable ${name} will not be printed because it is a l
@@ -19,3 +19,4 @@ Black Hat Bash by No Starch Press
 Variable  will not be printed because it is a l
 ocal variable.
 [Jose] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ 
+
