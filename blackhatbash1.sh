8b2c8a65610f6501df01c8c439ae206e06f3b4f7
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-03 13:09:18 +0000

    ultimate

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -x 
+#!/bin/bash
 #This line is called a shebang. It tells the system to run the script using Bash, and the -x option shows each command before it is executed for debugging purposes.
 set -x
 bash --version 
 env 
 echo ${SHELL} 
 echo ${RANDOM} 
-echo &{UID} 
+echo ${UID} 
 echo ${OSTYPE} 
 ps -e -f 
 ps -ef
@@ -13,7 +13,7 @@ df --human-readable
 bash -r myscript.sh #Runs the script myscript.sh in restricted Bash mode
 bash -n script.sh #Checks the script for syntax errors without executing it. It is useful for debugging Bash scripts safely.
 [Jose] UNIX-02-SIN-B-Mar-Jul-2026 # bash -n script.sh  
-script.sh: line 3: unexpected EOF while looking for matching `"'
+script.sh: line 3: unexpected EOF while looking for matching 
 [Jose] UNIX-02-SIN-B-Mar-Jul-2026 # 
 bash -x script.sh #Executes the script and displays each command before running it. It is mainly used for debugging and tracking how the script works step by step.
 set +x
