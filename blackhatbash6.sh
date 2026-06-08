5363eb0e9a88dfd1f6e6c55f928aa36265208f24
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-08 13:05:13 +0000

    ultimate for he moment

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-ls -l / grep "bin"
+ls -l / | grep "bin"
 SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}.."
 echo "Pinging the target: ${TARGET}..."
 ping "${TARGET}"
+chmod u+x ping_with_arguments.sh
+./ping_with_arguments.sh nostarch.com
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
+./bashhatbash6.sh "1" "2" "3"
