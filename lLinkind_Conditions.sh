b9171ea3f1aba3a10e8bdfc1d367a0c34ad080e8
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-10 12:53:42 +0000

    test 1234

--- lLinkind_Conditions.sh
+++ lLinkind_Conditions.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-DIR_NAME="dir_test"
-mkdir "${DIR_NAME}"
-if [[ -f "${DIR_NAME}" ]] || [[ -d "${DIR_NAME}" ]]; then
-echo "${DIR_NAME} is either a file or a directory."
+ if touch test1234; then
+echo "OK: file created"
 fi
