00b478533764c441f4d796d8eb1ef1977cf8bfa5
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-17 13:01:08 +0000

    bhb8.1.5

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -9,3 +9,9 @@ grep "42.236.10.117" log.txt | awk '{print $7}'
 sed 's/Mozilla/Godzilla/g' log.txt
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
 cat newlog.txt
+grep 's/Mozilla/Godzilla/g' newlog.txt
+sed 's/ //g' log.txt
+sed 's/ //g' log.txt > newlog1.txt
+grep Mozilla newlog1.txt
+grep Godzilla newlog1.txt
+sed '1d' log.txt
