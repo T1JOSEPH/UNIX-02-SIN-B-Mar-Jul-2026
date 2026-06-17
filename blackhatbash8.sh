3885683422ef3a767154d9b40c9de829626a5e0d
T1JOSEPH <jovacame@uide.edu.ec>
2026-06-17 12:44:29 +0000

    bhb8.1.3

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -3,4 +3,6 @@ awk '{print $1}' log.txt
 awk '{print $1,$2,$3}' log.txt
 awk '{print $1,$NF}' log.txt
 awk '{print $NF}' log.txt
+head log.txt
 awk 'NR < 10' log.txt
+grep "42.236.10.117" log.txt | awk '{print $7}'
